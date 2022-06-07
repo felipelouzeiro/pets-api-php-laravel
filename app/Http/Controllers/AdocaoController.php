e5bc290cc7e501ef3fd6a01b6f85d54c1217aa9f
felipe <fl.louzeiro@gmail.com>
2022-06-06 23:37:35 -0300

    feat: implementando validação de dados da request

--- app/Http/Controllers/AdocaoController.php
+++ app/Http/Controllers/AdocaoController.php
@@ -6,9 +6,22 @@ use App\Models\Adocao;
 use Illuminate\Http\Request;
 
 class AdocaoController extends Controller
+/**
+ * Cria um novo registro de adoção
+ *
+ * @param Request $request
+ * @return Adocao
+ */
 {
     public function store(Request $request)
     {
+
+        $request->validate([
+            'email' => ['required', 'email'],
+            'donation' => ['required', 'numeric', 'between:10,100'],
+            'pet_id' => ['required', 'int', 'exists:pets,id'],
+        ]);
+
         $adoptionData = $request->all();
 
         return Adocao::create($adoptionData);
